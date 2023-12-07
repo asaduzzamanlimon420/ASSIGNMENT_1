c67cd2304176ea60a5eb4375584bfcb0c147515f
asaduzzamanlimon420 <adilarian420@gmail.com>
2023-12-07 12:27:36 +0600

    2nd  commit

--- lib/main.dart
+++ lib/main.dart
@@ -49,4 +49,54 @@ class ProductDetailsPage extends StatelessWidget {
     Text(
     'About: The fragrance unfurls a powerful and noble trail.',
     style: TextStyle(fontSize: 16),
+    ),SizedBox(height: 32),
+      Builder(
+        builder: (context) => ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => BuyNowPage()),
+            );
+          },
+          child: Text('Buy Now'),
+        ),
+      ),
+      Text(
+        'Perfume Name',
+        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      ),
+      SizedBox(height: 16),
+      Image.network(
+        'https://manceraparfums.com/1045-grid_product_small_crop/amore-caffe.jpg',
+        height: 200,
+        width: 200,
+      ),
+      SizedBox(height: 16),
+      Text(
+        'Price: 1500.00 \৳',
+        style: TextStyle(fontSize: 18),
+      ),
+      SizedBox(height: 16),
+      Text(
+        'About:  Middle notes are Ice cream and Vanilla; base notes are Brown sugar, Vanilla and Ambergris.',
+        style: TextStyle(fontSize: 16),
+      ),
+      SizedBox(height: 32),
+      Builder(
+        builder: (context) => ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => BuyNowPage()),
+            );
+          },
+          child: Text('Buy Now'),
+        ),
+      ),
+    ],
     ),
+    ),
+        ),
+    );
+  }
+}
